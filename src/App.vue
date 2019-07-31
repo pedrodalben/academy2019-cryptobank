2650354292bf8ad3b6f6e0221f4dca7279829127
Pedro Henrique Dalben <pedro.morais3@fatec.sp.gov.br>
2019-07-31 13:14:12 -0300

    adicionado app.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  html,
+  body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100%;
+    background-color: #333333;
+  }
+</style>
